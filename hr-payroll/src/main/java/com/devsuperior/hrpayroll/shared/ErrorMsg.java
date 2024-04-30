cb1d88b1ac6e0202264b5a7cef4a7dbbaf70befa
fbrcmmelo <fbrcmmelo@gmail.com>
2024-04-29 22:50:13 -0300

    feat: inserted a new ms to eureka server registry

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/shared/ErrorMsg.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/shared/ErrorMsg.java
@@ -8,7 +8,7 @@ public class ErrorMsg implements Serializable {
 
     private Integer code;
     private Collection<String> messages;
-    
+
     public ErrorMsg(Integer code, Collection<String> messages) {
         this.code = code;
         this.messages = messages;
